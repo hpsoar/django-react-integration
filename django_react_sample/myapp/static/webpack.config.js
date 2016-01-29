7da87054b90512ca03da71ff18628c68df2dc5d7
huangpeng <huangpeng@chunyu.me>
2016-01-29 13:29:09 +0800

    fix package

--- django_react_sample/myapp/static/webpack.config.js
+++ django_react_sample/myapp/static/webpack.config.js
@@ -12,7 +12,11 @@ var config = {
 
     module: {
         loaders: [
-            {test: /\.js$/, loader: "jsx-loader?harmony" },
+          { test: /\.js$/, 
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel', // 'babel-loader' is also a legal name to reference
+            query: { presets: ['react', 'es2015'] }
+        },
         ]
     },
 
